543494f09f53bea30a9013fc3d136d9eb8715065
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-01-14 17:24:49 -0800

    dashboard/posts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -12,10 +12,18 @@ const withAuth = require("../utils/auth");
 // TODO - create logic for the GET route for / that renders the dashboard homepage
 // It should display all of the posts created by the logged in user
 router.get("/", withAuth, async (req, res) => {
-  // TODO - retrieve all posts from the database for the logged in user
-  // render the dashboard template with the posts retrieved from the database
-  //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
-  res.render("admin-all-posts", { layout: "dashboard" });
+  const postsData = await Post.findAll({
+     where: { userId: req.session.userId },
+     includes: [
+      {
+        model: User,
+        attributes: ["username"]
+      }
+     ],
+    });
+    const posts = postsData.map((post) => post.get({ plain: true}));
+
+  res.render("admin-all-posts", { layout: "dashboard", posts });
   // refer to admin-all-posts.handlebars write the code to display the posts
 });
 
