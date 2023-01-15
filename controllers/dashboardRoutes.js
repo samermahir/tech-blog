22d7136bc360884342d1fbe409b31d43ac498e31
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-01-14 16:53:58 -0800

    form box

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,8 +4,10 @@
 // All of these routes will be protected by the withAuth middleware function.
 
 const router = require("express").Router();
+// const { request } = require("express");
 const { Post } = require("../models/");
 const withAuth = require("../utils/auth");
+
 
 // TODO - create logic for the GET route for / that renders the dashboard homepage
 // It should display all of the posts created by the logged in user
@@ -16,6 +18,24 @@ router.get("/", withAuth, async (req, res) => {
   res.render("admin-all-posts", { layout: "dashboard" });
   // refer to admin-all-posts.handlebars write the code to display the posts
 });
+
+router.get("/create", withAuth, async (req, res) => {
+  res.render("admin-create-post", { layout: "dashboard" });
+});
+
+router.post("/create", withAuth, async (req, res) => {
+  try {
+  await Post.create({
+    title: req.body.title,
+    body: req.body.body,
+    userId: req.session.userId,
+  })
+
+  res.status(200).json({ message: "Post created successfully"})
+} catch (err) {
+  request.statusCode(500).json(err);
+}
+})
 
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
