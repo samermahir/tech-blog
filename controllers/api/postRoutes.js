8ed1f045665c7275e2d57851ddfb1bf48d8bb95d
Samer Mahir <samermahir@Samers-MacBook-Pro.local>
2023-01-19 20:15:51 -0800

    post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,7 +5,18 @@ const withAuth = require('../../utils/auth');
 
 // TODO - create a POST route for creating a new post
 // This should be a protected route, so you'll need to use the withAuth middleware
-
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 // TODO - create a PUT route for updating a post's title or body
 // This should be a protected route, so you'll need to use the withAuth middleware
@@ -13,5 +24,25 @@ const withAuth = require('../../utils/auth');
 
 // TODO - create a DELETE route for deleting a post with a specific id
 // This should be a protected route, so you'll need to use the withAuth middleware
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const deletePost = await Post.destroy({
+        where: {
+          id: req.params.id,
+          
+        },
+      });
+  
+      if (!deletePost) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(posttData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
